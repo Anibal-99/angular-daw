6caefbb85c36fdb3c5a53c07674f4b7c99aeeba1
anibal-99 <anibal_99_@hotmail.com>
2023-06-13 20:43:13 -0300

    nav-bar

---
+++ src/app/client-registration/client-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-registration',
+  templateUrl: './client-registration.component.html',
+  styleUrls: ['./client-registration.component.sass']
+})
+export class ClientRegistrationComponent {
+
+}
